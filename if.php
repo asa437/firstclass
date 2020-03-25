fb84c5a03c0a98a430aac4442d59f3f344bb35cc
unknown <klk437@yaho.com.tw>
2020-03-25 09:46:23 +0800

    if測試版

---
+++ if.php
@@ -0,0 +1,40 @@
+<?php
+// if 判斷式
+$score=70;
+if($score>=60){
+    echo "及格";
+}else{
+    echo "不及格";
+}
+echo "<hr>";
+
+//判斷成績
+$score=70;
+ echo"成績:" . $score;
+ echo"<br>";
+ echo"等級:";
+
+
+if($score>=90){
+    echo "A";
+}else if($score>=75){
+    echo "B";
+}else if($score>=60){
+    echo "C";
+}else {
+    echo "D";
+}
+
+
+
+
+
+
+
+echo "<hr>";
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,15 +1,88 @@
 <?php
 
-echo 'hello world';
 
-$aa;
-$bb;
+// 宣告一個變數
+$a=1;
+echo $a;
+echo "<br>";
 
- 
+$a=2.1;
+echo $a;
+echo "<br>";
+print($a);
+echo "<br>";
+
+$studentInSchool="YES";
+$_REQUEST;
+
+/*
+宣告一個常數
+常數不可被變更
+*/
+
+define("PI" , 3.1415927);
+echo PI;
+echo PI*2*10;
+
+echo "<br>";
+
+define("x",20);
+echo x;
+echo "<br>";
+echo x*5;
+
+echo "<hr>";
+//運算子
+//一元運算子 !
+$a=false;
+echo !$a;
+
+echo "<hr>";
+//運算子++ $b=$b+1
+$b=1;
+echo $b++;
+echo "<br>";
+echo $b+1 ;
+echo "<hr>";
+//算數運算子
+$a=2;
+$b=5;
+echo $a+$b;
+
+echo "<hr>";
+
+//子串運算子
+
+$a="A";
+$b="1";
+echo $a;
+echo "<br>";
+echo $b;
+$c=$a . $b ;
+$d=$a + $b ;
+echo "<hr>";
+echo $c; 
+echo "<hr>";
+echo $d; 
+
+echo "<hr>";
+
+//三元運算子
+
+$a=2;
+echo ($a>0)? "很貴":"免費";
+echo "<hr>";
+
+if($a>0){
+    echo "很貴";
+}
+else{
+    echo"免費;";
+}
+
+echo "<hr>";
+
+
+
 
 ?>
-
-<h1>hello world</h1>
-
-
-
